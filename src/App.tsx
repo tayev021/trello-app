c09facc60a10b7d12e7bfe0085d756cb6ea01687
Eugene Taranov <tayev021@gmail.com>
2025-08-12 21:53:30 +0300

    feat: add providers to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { Provider } from 'react-redux';
+import { RouterProvider } from 'react-router';
+import { store } from './store/store';
+import { router } from './pages/Router';
+
 export function App() {
-  return <div>Hello React</div>;
+  return (
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  );
 }

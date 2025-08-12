37261b7eaf348ff5d859c60ac5c0859109a7a3c9
Eugene Taranov <tayev021@gmail.com>
2025-08-12 21:43:20 +0300

    feat: add MenuContext

---
+++ src/context/MenuContext.ts
@@ -0,0 +1,12 @@
+import type { IPosition } from '../types/IPosition';
+import { createContext } from 'react';
+
+interface IMenuContext {
+  menuId: string;
+  open: (menuId: string) => void;
+  close: () => void;
+  position: IPosition | null;
+  setPosition: (position: IPosition) => void;
+}
+
+export const MenuContext = createContext<IMenuContext | null>(null);

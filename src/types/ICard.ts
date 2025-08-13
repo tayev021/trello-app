6bf79f7a730e25e1c00dae7499306a9c1c411094
Eugene Taranov <tayev021@gmail.com>
2025-08-13 20:56:06 +0300

    feat: update ICard type

--- src/types/ICard.ts
+++ src/types/ICard.ts
@@ -1,7 +1,9 @@
 import type { IBoard } from './IBoard';
+import type { ITask } from './ITask';
 
 export interface ICard {
   id: string;
   title: string;
   boardId: IBoard['id'];
+  tasks: ITask[];
 }

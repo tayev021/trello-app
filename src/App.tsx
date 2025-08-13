db13eed6ce400f72b1187f53f05a65c46e7d5e02
Eugene Taranov <tayev021@gmail.com>
2025-08-13 21:03:14 +0300

    refactor: add Modal provider to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router';
+import { Modal } from './components/Modal/Modal';
 import { store } from './store/store';
 import { router } from './pages/Router';
 
 export function App() {
   return (
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <Modal>
+        <RouterProvider router={router} />
+      </Modal>
     </Provider>
   );
 }

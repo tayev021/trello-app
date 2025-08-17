be3464a6b5e1ab6660bce6b62f53e4c343b569af
Eugene Taranov <tayev021@gmail.com>
2025-08-17 22:40:25 +0300

    feat: update Router

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,10 +1,20 @@
 import { createBrowserRouter } from 'react-router';
-import { HomePage } from './HomePage/HomePage';
 import { AppLayout } from './AppLayout';
+import { HomePage } from './HomePage/HomePage';
+import { BoardPage } from './BoardPage/BoardPage';
 
 export const router = createBrowserRouter([
   {
     Component: AppLayout,
-    children: [{ index: true, Component: HomePage }],
+    children: [
+      {
+        index: true,
+        Component: HomePage,
+      },
+      {
+        path: 'board/:boardId',
+        Component: BoardPage,
+      },
+    ],
   },
 ]);

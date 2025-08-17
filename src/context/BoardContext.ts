21afe8046ed3507545c6941e261d8f173cf4a70f
Eugene Taranov <tayev021@gmail.com>
2025-08-17 22:31:09 +0300

    feat: update BoardContext

--- src/context/BoardContext.ts
+++ src/context/BoardContext.ts
@@ -1,8 +1,11 @@
 import type { IBoard } from '../types/IBoard';
+import type { ICard } from '../types/ICard';
 import { createContext } from 'react';
 
 interface IBoardContext {
   board: IBoard;
+  cards: ICard[];
+  isDetailed: boolean;
 }
 
 export const BoardContext = createContext<IBoardContext | null>(null);

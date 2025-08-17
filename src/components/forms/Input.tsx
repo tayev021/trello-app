c8d3d280bed28fc088da16f88aa113d8283014df
Eugene Taranov <tayev021@gmail.com>
2025-08-17 22:34:45 +0300

    feat: add custom Input component

---
+++ src/components/forms/Input.tsx
@@ -0,0 +1,54 @@
+import { useState, type ChangeEvent, type FormEvent } from 'react';
+import { useOutsideClick } from '../../hooks/useOutsideClick';
+
+interface InputProps {
+  className: string;
+  initialValue: string;
+  onSubmit: (title: string) => void;
+  onClose: () => void;
+}
+
+export function Input({
+  className,
+  initialValue,
+  onSubmit,
+  onClose,
+}: InputProps) {
+  const [value, setValue] = useState(initialValue);
+  const ref = useOutsideClick<HTMLFormElement>(onClose);
+
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    setValue(event.target.value);
+  }
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (value.trim().length > 0) {
+      onSubmit(value.trim());
+      onClose();
+    }
+  }
+
+  return (
+    <form
+      ref={ref}
+      className="min-w-[150px] grid grid-cols-[1fr_min-content] items-center gap-2"
+      onSubmit={handleSubmit}
+    >
+      <input
+        className={'w-full  focus:outline-0 ' + className}
+        type="text"
+        value={value}
+        onChange={handleChange}
+        autoFocus
+      />
+      <button
+        className="py-1 px-2 rounded-xl leading-none uppercase hover:bg-[#FFFFFF] hover:shadow-[0_1px_3px_rgba(80,80,80,0.5)] text-sm cursor-pointer"
+        type="submit"
+      >
+        Update
+      </button>
+    </form>
+  );
+}

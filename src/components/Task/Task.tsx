0334a392f5dd02d73d1a3ff47b2ba5806e3d4af5
Eugene Taranov <tayev021@gmail.com>
2025-08-18 21:40:45 +0300

    feat: update Task component

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,10 +1,17 @@
+import { useState } from 'react';
+import { useBoardContext } from '../../hooks/useBoardContext';
 import { useDispatch } from 'react-redux';
-import { removeTask, toggleTaskStatus } from '../../store/cardsSlice';
 import type { ITask } from '../../types/ITask';
 import type { ICard } from '../../types/ICard';
-import { HiXMark } from 'react-icons/hi2';
-import styles from './Task.module.css';
+import {
+  updateTaskName,
+  toggleTaskStatus,
+  removeTask,
+} from '../../store/cardsSlice';
+import { Input } from '../forms/Input';
 import { Modal } from '../Modal/Modal';
+import { HiOutlinePencil, HiXMark } from 'react-icons/hi2';
+import styles from './Task.module.css';
 
 interface TaskProps {
   task: ITask;
@@ -12,7 +19,19 @@ interface TaskProps {
 }
 
 export function Task({ task, cardId }: TaskProps) {
+  const [isUpdatingName, setIsUpdatingName] = useState(false);
+  const { isDetailed } = useBoardContext();
   const dispatch = useDispatch();
+
+  const style = !isDetailed
+    ? { gridTemplateColumns: 'min-content 1fr min-content' }
+    : isUpdatingName
+    ? { gridTemplateColumns: '1fr', padding: '0' }
+    : {};
+
+  function handleNameUpdate(taskName: string) {
+    dispatch(updateTaskName({ id: cardId, taskId: task.id, name: taskName }));
+  }
 
   function handleToggleTaskStatus() {
     dispatch(toggleTaskStatus({ id: cardId, taskId: task.id }));
@@ -23,31 +42,58 @@ export function Task({ task, cardId }: TaskProps) {
   }
 
   return (
-    <li className="grid grid-cols-[min-content_1fr_min-content] gap-1 p-1 rounded-md text-base hover:bg-zinc-200">
-      <label className={styles['checkbox-container']}>
-        <input
-          type="checkbox"
-          checked={task.isDone}
-          onChange={handleToggleTaskStatus}
+    <li
+      className="grid grid-cols-[min-content_1fr_min-content_min-content] gap-1 p-1 rounded-md text-base hover:bg-zinc-200"
+      style={style}
+    >
+      {isUpdatingName ? (
+        <Input
+          className="py-[1px] pl-5 pr-2 border-b-2 border-blue-900 leading-none"
+          initialValue={task.name}
+          onSubmit={handleNameUpdate}
+          onClose={() => setIsUpdatingName(false)}
         />
-        <div className={styles['checkbox-mark']}></div>
-      </label>
+      ) : (
+        <>
+          <label className={styles['checkbox-container']}>
+            <input
+              type="checkbox"
+              checked={task.isDone}
+              onChange={handleToggleTaskStatus}
+            />
+            <div className={styles['checkbox-mark']}></div>
+          </label>
 
-      <p className="overflow-hidden whitespace-nowrap text-ellipsis leading-none">
-        {task.name}
-      </p>
+          <p className="border-b-2 border-transparent overflow-hidden whitespace-nowrap text-ellipsis leading-none">
+            {task.name}
+          </p>
+        </>
+      )}
 
-      <Modal.Open windowName={`remove-task-${task.id}`}>
-        <button className="hover:text-red-600 cursor-pointer">
-          <HiXMark />
-        </button>
-      </Modal.Open>
+      {!isUpdatingName && (
+        <>
+          {isDetailed && (
+            <button
+              className="hover:text-blue-700 cursor-pointer"
+              onClick={() => setIsUpdatingName(true)}
+            >
+              <HiOutlinePencil />
+            </button>
+          )}
 
-      <Modal.Window name={`remove-task-${task.id}`}>
-        <Modal.Confirm onConfirm={handleRemoveTask}>
-          Are you sure you want to remove "{task.name}" task?
-        </Modal.Confirm>
-      </Modal.Window>
+          <Modal.Open windowName={`remove-task-${task.id}`}>
+            <button className="hover:text-red-700 cursor-pointer">
+              <HiXMark />
+            </button>
+          </Modal.Open>
+
+          <Modal.Window name={`remove-task-${task.id}`}>
+            <Modal.Confirm onConfirm={handleRemoveTask}>
+              Are you sure you want to remove "{task.name}" task?
+            </Modal.Confirm>
+          </Modal.Window>
+        </>
+      )}
     </li>
   );
 }

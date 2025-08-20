fe17ab11059bd70ed99a26c542d5c922d67b636c
Eugene Taranov <tayev021@gmail.com>
2025-08-20 12:48:31 +0300

    feat: add updateBoardBGColor action to boardsSlice

--- src/store/boardsSlice.ts
+++ src/store/boardsSlice.ts
@@ -1,28 +1,34 @@
 import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
 import type { IBoardState } from '../types/IBoardState';
 import type { IBoard } from '../types/IBoard';
+import { BGColors, type IBGColors } from '../types/IBGColors';
 
 const initialState: IBoardState = {
   boards: [
     {
       id: '1',
       title: 'ToDo',
+      bgColor: BGColors[0],
     },
     {
       id: '2',
       title: 'In Progress',
+      bgColor: BGColors[0],
     },
     {
       id: '3',
       title: 'Done',
+      bgColor: BGColors[0],
     },
     {
       id: '4',
       title: 'Test 1',
+      bgColor: BGColors[0],
     },
     {
       id: '5',
       title: 'Test 2',
+      bgColor: BGColors[0],
     },
   ],
 };
@@ -30,6 +36,11 @@ const initialState: IBoardState = {
 interface IUpdateBoardTitlePayload {
   id: IBoard['id'];
   title: string;
+}
+
+interface IUpdateBoardBGColorPayload {
+  id: IBoard['id'];
+  color: IBGColors;
 }
 
 const boardsSlice = createSlice({
@@ -51,9 +62,23 @@ const boardsSlice = createSlice({
           : board
       );
     },
+    updateBoardBGColor(
+      state,
+      action: PayloadAction<IUpdateBoardBGColorPayload>
+    ) {
+      state.boards = state.boards.map((board) =>
+        board.id === action.payload.id
+          ? { ...board, bgColor: action.payload.color }
+          : board
+      );
+    },
   },
 });
 
 export const boardsReducer = boardsSlice.reducer;
-export const { createBoard, removeBoard, updateBoardTitle } =
-  boardsSlice.actions;
+export const {
+  createBoard,
+  removeBoard,
+  updateBoardTitle,
+  updateBoardBGColor,
+} = boardsSlice.actions;

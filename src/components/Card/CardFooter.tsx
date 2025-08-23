c21dcf5d2f5987ca6f0b57f9b76c9ea48855c536
Eugene Taranov <tayev021@gmail.com>
2025-08-23 08:21:18 +0300

    refactor: update CardFooter component

--- src/components/Card/CardFooter.tsx
+++ src/components/Card/CardFooter.tsx
@@ -1,49 +1,24 @@
-import { useState, type ChangeEvent, type FormEvent } from 'react';
 import { useCardContext } from '../../hooks/useCardContext';
-import { useOutsideClick } from '../../hooks/useOutsideClick';
 import { useDispatch } from 'react-redux';
 import { createTask } from '../../store/cardsSlice';
+import { Input } from '../forms/Input';
 
 export function CardFooter() {
-  const [taskNameValue, setTaskNameValue] = useState('');
   const { card, setIsAddingTask } = useCardContext();
-  const ref = useOutsideClick<HTMLFormElement>(() => setIsAddingTask(false));
   const dispatch = useDispatch();
 
-  function handleChange(event: ChangeEvent<HTMLInputElement>) {
-    setTaskNameValue(event.target.value);
-  }
-
-  function handleSubmit(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-
-    if (taskNameValue.length > 0) {
-      dispatch(createTask({ id: card.id, name: taskNameValue }));
-      setIsAddingTask(false);
-    }
+  function handleSubmit(taskName: string) {
+    dispatch(createTask({ id: card.id, name: taskName }));
   }
 
   return (
     <footer className="mt-2 mb-1">
-      <form
-        ref={ref}
-        className="grid grid-cols-[1fr_min-content] items-center gap-2"
+      <Input
+        className="w-full py-0.5 px-3 rounded-2xl border-zinc-600  focus:outline-0 font-normal text-sm leading-0 bg-[#FFFFFF] shadow-[0_1px_3px_rgba(60,60,60,0.5)]"
+        buttonTitle="Add"
         onSubmit={handleSubmit}
-      >
-        <input
-          className="w-full py-0.5 px-3 rounded-2xl border-zinc-600  focus:outline-0 font-normal text-sm leading-0 bg-[#FFFFFF] shadow-[0_1px_3px_rgba(60,60,60,0.7)]"
-          type="text"
-          value={taskNameValue}
-          onChange={handleChange}
-          autoFocus
-        />
-        <button
-          className="py-1 px-4 rounded-2xl leading-none uppercase hover:bg-[#FFFFFF] hover:shadow-[0_1px_3px_rgba(60,60,60,0.7)] text-sm cursor-pointer"
-          type="submit"
-        >
-          Add
-        </button>
-      </form>
+        onClose={() => setIsAddingTask(false)}
+      />
     </footer>
   );
 }

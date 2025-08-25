fba5db7d1b6b054ebe8e55e7f0a98c9be7082ea7
Eugene Taranov <tayev021@gmail.com>
2025-08-25 20:38:42 +0300

    feat: update AppLayout component

--- src/pages/AppLayout.tsx
+++ src/pages/AppLayout.tsx
@@ -3,12 +3,24 @@ import { Link, Outlet } from 'react-router';
 export function AppLayout() {
   return (
     <div className="h-screen grid grid-rows-[min-content_1fr] bg-blue-400 bg-[url(/src/assets/bg.svg)] bg-size-[150px] overflow-hidden select-none">
-      <header className="py-4 px-6 bg-[#F1F2F4] shadow-[0_3px_8px_rgba(0,0,0,0.4)] z-40">
+      <header className="flex justify-between g-4 py-4 px-6 bg-[#F1F2F4] shadow-[0_3px_8px_rgba(0,0,0,0.4)] z-40">
         <Link to="/">
           <h1 className="font-medium text-xl capitalize leading-none text-blue-900 tracking-wider">
             Trello App
           </h1>
         </Link>
+        <nav>
+          <ul className="flex gap-4">
+            <li>
+              <Link
+                to="statistic"
+                className="border-b-2 border-transparent hover:border-blue-900 text-lg leading-none text-blue-900"
+              >
+                Statistic
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </header>
       <main className="py-7 px-5 overflow-y-auto">
         <Outlet />

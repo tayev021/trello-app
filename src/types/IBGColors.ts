411a209a5893377a9f54e75b43d69bdc284402b3
Eugene Taranov <tayev021@gmail.com>
2025-08-25 20:27:58 +0300

    feat: update IBGColors types

--- src/types/IBGColors.ts
+++ src/types/IBGColors.ts
@@ -9,6 +9,17 @@ export const BGColors = [
   '#cbd5e1',
 ] as const;
 
+export const BGColorsBrighter = {
+  [BGColors[0]]: '#71717a',
+  [BGColors[1]]: '#dc2626',
+  [BGColors[2]]: '#ea580c',
+  [BGColors[3]]: '#facc15',
+  [BGColors[4]]: '#22c55e',
+  [BGColors[5]]: '#0ea5e9',
+  [BGColors[6]]: '#7c3aed',
+  [BGColors[7]]: '#334155',
+};
+
 type BGColorsTuple = typeof BGColors;
 
 export type IBGColors = BGColorsTuple[number];
